32c29949bf78d14c8c0d5bff7d4e85d2ceb55917
hdssong <dev.hdss@gmail.com>
2025-06-23 18:36:32 +0900

    first commit

---
+++ backend/src/main/java/kr/go/knp_system/KnpBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.go.knp_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnpBackendApplication.class, args);
+	}
+
+}

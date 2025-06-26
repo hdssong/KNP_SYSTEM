1ebeb52831857c0b2b71751af7d7b6db8457ac50
hdssong <dev.hdss@gmail.com>
2025-06-26 09:50:33 +0900

    LDAP Test 추가

--- backend/src/main/java/kr/go/knp_system/Controller/HomeController.java
+++ backend/src/main/java/kr/go/knp_system/Controller/HomeController.java
@@ -1,10 +1,11 @@
 package kr.go.knp_system.Controller;
 
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class MainController {
+@RestController
+public class HomeController {
 
     @GetMapping("/")
     public String index(){

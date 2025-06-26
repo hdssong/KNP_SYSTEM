a7214f23ce34beff59ba90db60356fcf1e0026eb
hdssong <dev.hdss@gmail.com>
2025-06-26 12:52:09 +0900

    LDAP Test 구현, ldif추가, properties 추가

--- backend/src/main/java/kr/go/knp_system/Controller/HomeController.java
+++ backend/src/main/java/kr/go/knp_system/Controller/HomeController.java
@@ -1,10 +1,10 @@
 package kr.go.knp_system.Controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class MainController {
+@RestController
+public class HomeController {
 
     @GetMapping("/")
     public String index(){
